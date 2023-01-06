e38d5388e31904e4d8a67833b3509c2b24a414e6
Muhammad Fauzi <muhammadfauzi.2.mf52@gmail.com>
2023-01-07 01:42:48 +0700

    update button

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ export default function Header () {
         </div>
 
         <div className="flex mb-4 lg:flex-row gap-2 md:flex flex-col  mt-4 ">
-          <Link href="https://github.com/iamfauzi16" target="_blank"> 
+          <Link href="https://github.com/iamfauzi16" target="_blank" className='flex  lg:flex-row  md:flex flex-col'>  
             <button className="font-semibold p-2 px-4 hover:bg-indigo-800 rounded-lg  focus:ring-2 ring-indigo-600  bg-indigo-600">Github</button>
           </Link>
           <button className="font-semibold p-2 px-4 hover:bg-orange-800 rounded-lg  focus:ring-2 ring-indigo-600  bg-orange-600">Download</button>
